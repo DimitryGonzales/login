b4fa3df47a0a7e341a3958556bc4821f9772b1e9
DimitryGonzales <125162224+DimitryGonzales@users.noreply.github.com>
2026-09-17 22:49:48 -0300

    fix: add access permission for the js and models folders

--- src/main/java/com/example/Login/config/SecurityConfig.java
+++ src/main/java/com/example/Login/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/css/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/images/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/js/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/models/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/recoverpassword").permitAll()
